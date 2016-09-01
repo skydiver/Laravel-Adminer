8be47d9cf87b302273962f25935d0309fa728839
Martin <skydiver@users.noreply.github.com>
2016-09-01 14:56:12 -0300

    Update to Adminer 4.2.5

--- src/AdminerController.php
+++ src/AdminerController.php
@@ -7,10 +7,10 @@
     class AdminerController extends Controller {
 
         public function index() {
-            require('adminer-4.2.3-en.php');
+            require('adminer-4.2.5-en.php');
             return new EmptyResponse();
         }
 
     }
-    
+
 ?>
